bd88d4e837be03c5556de88e8d6c0e08f5ec1df3
EC2 Default User <ec2-user@ip-172-31-34-95.us-east-2.compute.internal>
2018-05-24 06:24:07 +0000

    Added Static Page Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-      render html: "Hello Godwin!"
+      render html: "Hello From Godwin App!"
   end
 end
