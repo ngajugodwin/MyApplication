2ff8222225243cfcd43c7206fb3261ac053959d9
ngajugodwin <ngajugodwin@yahoo.com>
2018-05-31 02:23:12 +0000

    Basic Login Implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
   
-  def hello
-      render html: "Hello From Godwin App!"
-  end
 end
